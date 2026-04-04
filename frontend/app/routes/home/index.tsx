ab86629a8bee675946e66b7e2a974c34b3102206
carolmedici <carolina.obstetriz@gmail.com>
2026-04-04 16:09:44 -0300

    feat: add professional profile section with tech stack and bio

--- frontend/app/routes/home/index.tsx
+++ frontend/app/routes/home/index.tsx
@@ -1,4 +1,7 @@
 import type { Route } from './+types/index';
+import { useLanguage } from "../../context/LanguageContext";
+import en from "../../locales/en.json";
+import pt from "../../locales/pt.json";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +11,44 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <>
-  My App</>;
+
+  const {lang} = useLanguage();
+  const t = lang === "en" ? en : pt;
+
+  return (
+    <main>     
+      <section className="bg-white  px-6">
+        <div className="max-w-4xl mx-auto flex flex-col md:flex-row items-center gap-12">          
+            <div className="w-48 h-48 rounded-full bg-gray-200 overflow-hidden shadow-lg">
+              <img src="../public/images/profile.png" alt="Carolina Médici" className="w-full h-full object-cover" />
+            </div>
+
+          <div className="flex-1">
+            <h2 className="text-3xl font-bold text-gray-900 mb-6">{t.about.title}</h2>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              {t.about.text}
+            </p>            
+          
+            <div className="mt-8 flex flex-wrap gap-3">
+              <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm font-semibold">Java / Spring Boot</span>
+              <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm font-semibold">REST APIs</span>
+              <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm font-semibold">JUnit</span>
+
+              <span className="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm font-semibold">Angular</span>
+              <span className="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm font-semibold">React</span>
+              <span className="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm font-semibold">Tailwind</span>
+              <span className="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm font-semibold">Jasmine</span>
+
+              <span className="px-3 py-1 bg-emerald-100 text-emerald-700 rounded-full text-sm font-semibold">PostgreSQL</span>
+              <span className="px-3 py-1 bg-emerald-100 text-emerald-700 rounded-full text-sm font-semibold">MySQL</span>
+              <span className="px-3 py-1 bg-emerald-100 text-emerald-700 rounded-full text-sm font-semibold">Git</span>
+              <span className="px-3 bg-emerald-100 text-emerald-700 rounded-full text-sm font-semibold">Keycloack</span>
+              <span className="px-3 py-1 bg-emerald-100 text-emerald-700 rounded-full text-sm font-semibold">CI/CD Pipelines</span>
+
+            </div>
+          </div>
+        </div>
+      </section>    
+    </main>
+  );
 }
